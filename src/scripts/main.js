53f6252b6908d64f1a9d332847c73026fe74c15f
Wessel Smit <wrsmit00@gmail.com>
2020-05-15 17:24:09 +0200

    github logo and hover state

---
+++ src/scripts/main.js
@@ -0,0 +1,4 @@
+const github = document.getElementById('github')
+
+github.addEventListener('mouseover', () => github.src = "src/media/images/github_logo_blue.svg")
+github.addEventListener('mouseout', () => github.src = "src/media/images/github_logo_black.svg")
