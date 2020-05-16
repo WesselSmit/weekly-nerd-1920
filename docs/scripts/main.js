5fb153518b0183b93498988c8bae651606e9888b
Wessel Smit <wrsmit00@gmail.com>
2020-05-16 21:12:51 +0200

    more link changes

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,5 +1,5 @@
 //Change github icon on hover
 const github = document.getElementById('github')
 
-github.addEventListener('mouseover', () => github.src = "src/media/images/github_logo_blue.svg")
-github.addEventListener('mouseout', () => github.src = "src/media/images/github_logo_black.svg")
+github.addEventListener('mouseover', () => github.src = "docs/media/images/github_logo_blue.svg")
+github.addEventListener('mouseout', () => github.src = "docs/media/images/github_logo_black.svg")
